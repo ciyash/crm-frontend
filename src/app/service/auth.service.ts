b6b37928043978862875759cd77c91f776b8b3de
PavanKumar123230 <reddaboinapavan49@gmail.com>
2025-10-14 15:15:15 +0530

    changes

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,12 +1,20 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
- 
-const AUTH_API = 'https://cargo-backend-bpq4.onrender.com/';
-const login_API = 'https://cargo-backend-bpq4.onrender.com/';
+
+// chandu api 
+const AUTH_API = 'https://cargo-logistics-zsqp.onrender.com/';
+const login_API = 'https://cargo-logistics-zsqp.onrender.com/';
+                      // new api
+// const login_API = 'https://cargobackend-9kxq.onrender.com/';
+// const AUTH_API = 'https://cargobackend-9kxq.onrender.com/';
+
 
 // const AUTH_API = 'http://3.109.182.152:4000/';
 // const login_API = 'http://3.109.182.152:4000/';
+
+
+
 
  
 const httpOptions = {
